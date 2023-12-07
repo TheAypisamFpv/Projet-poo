0a3752566be3cdef147c5edf7a0a592e423efeb7
the aypisam fpv <the.aypisam.64@gmail.com>
2023-12-07 14:35:32 +0100

    updated controller

--- Source Code/controller.cpp
+++ Source Code/controller.cpp
@@ -1,18 +1,24 @@
 #include "link/link.h"
-#include "user/ui/ui.h"
-#include "user/user.h"
-
 #include "string.h"
 
 using std::string;
 
+
+
+
 string Table(string request) {
+	//						   arg1   :  arg2 :  arg3(param1,param2,...)
 	// normalized request : wich_table:command:parameters
 	// exemple : staff:delete:cailloux,rock (delete staff name rock cailloux)
+	string data;
 
 	//check request by checking if there is 2 ':'
-	if (count(request.begin(), request.end(), ':') != 2) {
-		return "error:invalid_request:wrong_number_argument";
+	argument_number = count(request.begin(), request.end(), ':');
+	if (argument_number > 2) {
+		return "error:invalid_argument:too_many_arguments";
+	}
+	else if (argument_number < 2) {
+		return "error:invalid_argument:too_few_arguments";
 	}
 
 	// split request
@@ -22,19 +28,44 @@ string Table(string request) {
 
 	// check if none is empty
 	if (table.empty() || command.empty() || parameters.empty()) {
-		return "error:invalid_request:empty_arguments";
+		return "error:invalid_argument:empty_argument";
 	}
 
+
+	// if there is more than one parameter, check if both are not empty
+	if (count(parameters.begin(), parameters.end(), ',') > 0) {
+		if (parameters.substr(0, parameters.find(",")) == "" || parameters.substr(parameters.find(",") + 1) == "") {
+			return "error:invalid_parameter:empty_parameter";
+		}
+	}
+
+
+
+
+	return data;
 }
 
 
 
 string Stats(string request) {
+	//						  arg1 :  arg2(param1,param2,...)
 	// normalized request : command:parameters
 	// exemple : total_purchases:cailloux (get total purchases of user cailloux)
 
+	string data;
+
 	//check request by checking if there is 1 ':'
-	if (count(request.begin(), request.end(), ':') != 1) {
-		return "error:invalid_request";
+	argument_number = count(request.begin(), request.end(), ':');
+	if (argument_number > 1) {
+		return "error:invalid_argument:too_many_arguments";
 	}
+	else if (argument_number < 1) {
+		return "error:invalid_argument:too_few_arguments";
+	}
+
+
+
+
+
+	return data
 }
---
+++ Source Code/link/link.h
@@ -0,0 +1,1 @@
+//include everything needed to connect to a sql server
