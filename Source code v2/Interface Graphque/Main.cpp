a25739d6f35402f2ac5113f4ed88ce00d0ce917b
Trifyd <53906573+Trifyd@users.noreply.github.com>
2023-12-11 09:11:15 +0100

    winmain to main

--- Source code v2/Interface Graphque/Main.cpp
+++ Source code v2/Interface Graphque/Main.cpp
@@ -4,10 +4,11 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
-int __clrcall WinMain(cli::array<System::String ^>^ args)
+int __clrcall Main(cli::array<System::String ^>^ args)
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	InterfaceGraphque::MyForm monFormulaire;
 	Application::Run(% monFormulaire);
+	return 1;
 }
