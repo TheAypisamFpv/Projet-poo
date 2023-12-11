ff8a1f70a4ec6654ea4c2c86a92b42a722330cb0
the aypisam fpv <the.aypisam.64@gmail.com>
2023-12-11 09:25:51 +0100

    bug fix

--- Source code v2/Interface Graphque/link.cpp
+++ Source code v2/Interface Graphque/link.cpp
@@ -13,13 +13,13 @@ System::Data::DataSet^ link::hub(string table, string command, string parameters
 	if (table == "client") {
 		client c;
 		if (command == "create") {
-			return c.create(parameters);
+			c.create(parameters);
 		}
 		else if (command == "modify") {
-			return c.modify(parameters);
+			c.modify(parameters);
 		}
 		else if (command == "delete") {
-			return c.delete_(parameters);
+			c.delete_(parameters);
 		}
 		else if (command == "show") {
 			return c.show(parameters);
@@ -28,13 +28,13 @@ System::Data::DataSet^ link::hub(string table, string command, string parameters
 	else if (table == "staff") {
 		staff s;
 		if (command == "create") {
-			return s.create(parameters);
+			s.create(parameters);
 		}
 		else if (command == "modify") {
-			return s.modify(parameters);
+			s.modify(parameters);
 		}
 		else if (command == "delete") {
-			return s.delete_(parameters);
+			s.delete_(parameters);
 		}
 		else if (command == "show") {
 			return s.show(parameters);
@@ -43,13 +43,13 @@ System::Data::DataSet^ link::hub(string table, string command, string parameters
 	else if (table == "order") {
 		order o;
 		if (command == "create") {
-			return o.create(parameters);
+			o.create(parameters);
 		}
 		else if (command == "modify") {
-			return o.modify(parameters);
+			o.modify(parameters);
 		}
 		else if (command == "delete") {
-			return o.delete_(parameters);
+			o.delete_(parameters);
 		}
 		else if (command == "show") {
 			return o.show(parameters);
@@ -58,13 +58,13 @@ System::Data::DataSet^ link::hub(string table, string command, string parameters
 	else if (table == "stock") {
 		stock s;
 		if (command == "create") {
-			return s.create(parameters);
+			s.create(parameters);
 		}
 		else if (command == "modify") {
-			return s.modify(parameters);
+			s.modify(parameters);
 		}
 		else if (command == "delete") {
-			return s.delete_(parameters);
+			s.delete_(parameters);
 		}
 		else if (command == "show") {
 			return s.show(parameters);
